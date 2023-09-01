d7e95f73fc2399c3e10256070f07ce1fb18c7b16
BordekarDarshan <bordekardarshan@gmail.com>
2023-09-02 01:46:57 +0530

    1. created root component for campaigns

---
+++ src/features/campaigns/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CampaignList from "../../components/CampaignList";
+import Search from "../../components/Search";
+
+function Campaigns() {
+  return (
+    <div>
+      <Search />
+      <CampaignList />
+    </div>
+  );
+}
+
+export default Campaigns;
